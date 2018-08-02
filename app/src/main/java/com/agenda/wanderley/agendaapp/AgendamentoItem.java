6eb5d6ca6eed0f189905f5421bfad4a96bc0052c
derleyferreira <derleyes@yahoo.com.br>
2018-08-01 22:01:15 -0300

    Atualização

---
+++ app/src/main/java/com/agenda/wanderley/agendaapp/AgendamentoItem.java
@@ -0,0 +1,18 @@
+package com.agenda.wanderley.agendaapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AgendamentoItem extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agendamento_item);
+
+
+    }
+
+}
+
---
+++ app/src/main/java/com/agenda/wanderley/agendaapp/HistoricoItem.java
@@ -0,0 +1,16 @@
+package com.agenda.wanderley.agendaapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class HistoricoItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historico_item);
+
+
+    }
+
+}
